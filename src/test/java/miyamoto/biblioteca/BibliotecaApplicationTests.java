09e1050570c1b58ca570d3e547132ae567e45339
hkojim <eng.koji@gmail.com>
2026-02-20 13:36:57 -0300

    criando biblioteca

---
+++ src/test/java/miyamoto/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package miyamoto.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
