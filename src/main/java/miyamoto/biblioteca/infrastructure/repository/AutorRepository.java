7b6a678ca6bd7fe558fc53d32e3596a28440a234
hkojim <eng.koji@gmail.com>
2026-02-21 12:40:34 -0300

    adicionado os services, controllers e o JWT foi aproveitado de outro projeto

--- src/main/java/miyamoto/biblioteca/infrastructure/repository/AutorRepository.java
+++ src/main/java/miyamoto/biblioteca/infrastructure/repository/AutorRepository.java
@@ -4,6 +4,6 @@ import miyamoto.biblioteca.infrastructure.entity.AutorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //CRUD de Autor.
-
 public interface AutorRepository extends JpaRepository<AutorEntity, Long> {
+    boolean existsByEmail(String email);
 }
---
+++ src/main/java/miyamoto/biblioteca/infrastructure/security/dto/ErrorResponseDTO.java
@@ -0,0 +1,21 @@
+package miyamoto.biblioteca.infrastructure.security.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ErrorResponseDTO {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+
+}
